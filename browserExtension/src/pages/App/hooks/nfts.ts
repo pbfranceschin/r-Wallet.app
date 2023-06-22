bf1882f42c4e7ef7b0434dac9f3b3cf2801c6424
bfranceschin <bfranceschin@gmail.com>
2023-06-22 00:22:41 -0300

    feat: first hooks and dashboard tests

---
+++ browserExtension/src/pages/App/hooks/nfts.ts
@@ -0,0 +1,186 @@
+import axios, { Axios } from 'axios'
+import { ethers } from "ethers";
+import { useEffect, useState, useRef } from "react";
+// import { useRouter } from 'next/router'
+import {
+  useContractRead,
+  useContract,
+  useProvider,
+} from "wagmi";
+
+import {
+  getContractData,
+  getEthersNftContract,
+  ipfsToHTTP,
+  getTokenMetadata,
+} from '../../../utils'
+
+const [contractAddress, contractABI] = getContractData();
+
+// export const useTokenUri = (tokenId) => {
+//   const [tokenUri, setTokenUri] = useState()
+//   const [tokenUriError, setTokenUriError] = useState()
+//   if (!tokenUri && !tokenUriError) {
+//       const { data, isError, isLoading } = useContractRead({
+//       address: contractAddress,
+//       abi: contractABI,
+//       functionName: 'tokenURI',
+//       args: [tokenId],
+//     })
+//     console.log(data, isError, isLoading)
+//     if (!isLoading && !isError) {
+//       console.log("data",data)
+//       setTokenUri(data);
+//     }
+//     else if (isError) {
+//       console.log("isError")
+//       console.log(isError)
+//       setTokenUriError(isError)
+//     }
+//   }
+//   return tokenUri
+// }
+
+export const useTokenUri = (tokenId: number) : any => {
+  const [tokenUri, setTokenUri] = useState()
+  const provider = useProvider()
+  useEffect(() => {
+    if (provider) {
+      const nftContract = getEthersNftContract(provider)
+      nftContract.tokenURI(tokenId).
+      then ( (uri: any) =>{
+        setTokenUri(uri)
+      })
+    }
+  },[provider, tokenId])
+  return tokenUri
+}
+
+export const useTokenCount = () : number => {
+  const provider = useProvider()
+  const [contractAddress, contractABI] = getContractData();
+  const { data, isError, isLoading } = useContractRead({
+    address: contractAddress as `0x${string}`,
+    abi: contractABI as any,
+    functionName: 'totalSupply',
+    watch: true,
+  })
+  if (isError || isLoading)
+    return 0;
+  return ethers.BigNumber.from(data).toNumber();
+}
+    
+
+export const useTokenMetaData = (tokenId: number) : any => {
+  const [tokenMetadata, setTokenMetadata] = useState()
+  const tokenUri = useTokenUri(tokenId)
+  useEffect(() => {
+    if (tokenUri) {
+      axios.get(ipfsToHTTP(tokenUri)).
+      then(request => {
+        console.log(request.data)
+        // transform data from string to object
+        // const data = JSON.parse(request.data)
+        const data = request.data;
+        setTokenMetadata(data)
+      })
+    }
+  },[tokenUri])
+  return tokenMetadata
+}
+
+// export const useAllMetadata = () => {
+//   const [allMetadata, setAllMetadata] = useState([])
+//   const requests = useRef({})
+//   const provider = useProvider()
+//   const [contractAddress, contractABI] = getContractData();
+//   const { data: tokenCount, isError, isLoading } = useContractRead({
+//     address: contractAddress as `0x${string}`,
+//     abi: contractABI as any,
+//     functionName: 'numberOfTokens',
+//     watch: true,
+//   })
+
+//   useEffect(() => {
+//     if (!tokenCount) {
+//       return
+//     }
+//     const count = tokenCount as number;
+//     if (count > allMetadata.length) {
+//       for (let i=allMetadata.length; i<count; i++) {
+//         if (!requests.current[i]) {
+//           requests.current[i] = getTokenMetadata(provider, i).then(
+//             response => {
+//               console.log(i, response)
+//               // allMetadata.push()
+//               setAllMetadata(arr => {
+//                 const ret = [...arr, {tokenId: i.toString(), metadata: response.data}]
+//                 ret.sort((e1, e2) => e2.tokenId - e1.tokenId)
+//                 return ret
+//               })
+//             }
+//           ) 
+//         }
+        
+//       }
+//     }
+//   },[tokenCount])
+
+//   return allMetadata
+// }
+
+export const useTokenImage = (tokenId: number) : string => {
+  const metadata = useTokenMetaData(tokenId)
+//   console.log(metadata)
+//   if (metadata)
+//     console.log("image ",typeof metadata)
+//   console.log("---------------------")
+  return metadata ? ipfsToHTTP(metadata.image as string) : "";
+}
+
+const isAccountToken = async (provider: any, accountAddress: string, tokenId: number) => {
+  const nftContract = getEthersNftContract(provider)
+  const owner = await nftContract.ownerOf(tokenId);
+  return owner === accountAddress;
+}
+
+const buidAccountTokens = async (provider: any, accountAddress: string, setAccountTokens: any) => {
+  const nftContract = getEthersNftContract(provider)
+  const tokenCount = await nftContract.totalSupply();
+  let promises = [];
+  const init = 5
+  for (let i = init; i < tokenCount; i++) {
+    promises.push(isAccountToken(provider, accountAddress, i));
+  }
+  const tokensBools = await Promise.all(promises);
+  const tokens = tokensBools.map((p, i) =>  p ? i+init : -1);
+  const tokensFiltered = tokens.filter(t => t > 0);
+  setAccountTokens(tokensFiltered);
+}
+
+export const useAccountTokens = (accountAdress: string) : number[] => {
+  const [accountTokens, setAccountTokens] = useState<number[]>([])
+  const provider = useProvider()
+  const [contractAddress, contractABI] = getContractData();
+  const { data: accountBalance, isError, isLoading } = useContractRead({
+    address: contractAddress as `0x${string}`,
+    abi: contractABI as any,
+    functionName: 'balanceOf',
+    args: [accountAdress],
+    watch: true,
+  })
+
+  useEffect(() => {
+    if (!provider) {
+      return
+    }
+    buidAccountTokens(provider, accountAdress, setAccountTokens);
+  },[accountBalance])
+  return accountTokens
+}
+
+// export function useTokenId () {
+//   const router = useRouter()
+//   const {tokenId} = router.query
+//   return tokenId
+// }
--- browserExtension/src/pages/App/pages/home/home.tsx
+++ browserExtension/src/pages/App/pages/home/home.tsx
@@ -14,9 +14,35 @@ import AccountInfo from '../../components/account-info';
 import Header from '../../components/header';
 import TransferAssetButton from '../../components/transfer-asset-button';
 import { useBackgroundSelector } from '../../hooks';
+import { getContractData } from '../../../../utils'
+import { useTokenCount, useTokenUri, useTokenImage, useTokenMetaData, useAccountTokens } from '../../hooks/nfts'
+
+const TempImage = ({tokenId}: {tokenId: number}) => {
+  const image: string = useTokenImage(tokenId);
+  return (
+    <img src={image} />
+  );
+}
 
 const Home = () => {
   const activeAccount = useBackgroundSelector(getActiveAccount);
+
+  // console.log("getContractData", getContractData())
+  const tokenCount = useTokenCount();
+  console.log("tokenCount", tokenCount)
+  // const uri = useTokenUri(1)
+  // console.log("uri", uri);
+  // const tokenMetaData = useTokenMetaData(1);
+  // console.log("tokenMetaData", tokenMetaData)
+  // const tokenImage = useTokenImage(5);
+  // console.log("useTokenImage", tokenImage)
+  let tokens = [];
+  for (let i = 5; i < tokenCount; i++) {
+    tokens.push(i);
+  }
+
+  const accountTokens = useAccountTokens(activeAccount? activeAccount : '');
+  console.log(accountTokens);
 
   return (
     <Container sx={{ width: '62vw', height: '100vh' }}>
@@ -46,6 +72,13 @@ const Home = () => {
           </Box>
         </CardContent>
       </Card>
+      {
+        tokens.map((tokenId: number) => {
+          return (
+            <TempImage tokenId={tokenId} />
+          );
+        })
+      }
     </Container>
   );
 };
